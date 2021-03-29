113de0c8312794f97fbd631f6ba4cf88e870dbf3
Colbyn Wadman <hello@colbyn.com>
2021-03-29 17:18:31 -0600

    save

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 set -e
 
-mkdir -p output
-tectonic \
-    --outdir output \
-    research-paper/research-paper.tex
+# mkdir -p output
+# tectonic \
+#     --outdir output \
+#     research-paper/research-paper.tex
 
-tectonic \
-    --outdir output \
-    bibliography/bibliography.tex
+# tectonic \
+#     --outdir output \
+#     bibliography/bibliography.tex
 
 tectonic \
     --outdir output \
